5349512e97ebdd18d14dab6e5d85bcddfa459c98
Sean Chen <seanchen11235@gmail.com>
2021-03-30 11:20:02 -0500

    Original minigrep implementation

--- examples/minigrep/src/bin/main.rs
+++ examples/minigrep/src/bin/main.rs
@@ -4,15 +4,12 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let config = match Config::new(env::args()) {
-        Ok(config) => config,
-        Err(err) => {
-            eprintln!("Parse error: {}", err);
-            process::exit(1);
-        }
-    };
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);   
+        process::exit(1);
+    });
 
-    if let Err(err) = config.run() {
+    if let Err(err) = minigrep::run(config) {
         eprintln!("Application error: {}", err);
         process::exit(1);
     }
--- examples/minigrep/src/lib.rs
+++ examples/minigrep/src/lib.rs
@@ -1,53 +1,51 @@
-mod error;
-
-use error::AppError;
 use std::env;
 use std::fs;
+use std::error::Error;
 
 pub struct Config {
-    pattern: String,
+    query: String,
     filename: String,
-    case_insensitivity: bool,
+    case_sensitive: bool,
 }
 
 impl Config {
-    pub fn new(mut args: env::Args) -> Result<Self, AppError> {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         args.next();
 
-        let pattern = match args.next() {
+        let query = match args.next() {
             Some(arg) => arg,
-            None => return Err(AppError::MissingPattern),
+            None => return Err("Didn't get a query string"),
         };
 
         let filename = match args.next() {
             Some(arg) => arg,
-            None => return Err(AppError::MissingFilename),
+            None => return Err("Didn't get a file name"),
         };
 
-        let case_insensitivity = env::var("CASE_INSENSITIVE").is_ok();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {
-            pattern,
+            query,
             filename,
-            case_insensitivity,
+            case_sensitive,
         })
     }
+}
 
-    pub fn run(&self) -> Result<(), AppError> {
-        let contents = fs::read_to_string(&self.filename)?;
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
-        let found = if self.case_insensitivity {
-            search_insensitive(&self.pattern, &contents)
-        } else {
-            search(&self.pattern, &contents)
-        };
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
 
-        for line in found {
-            println!("{}", line);
-        }
-
-        Ok(())
+    for line in results {
+        println!("{}", line);
     }
+
+    Ok(())
 }
 
 pub fn search<'a>(pattern: &str, contents: &'a str) -> Vec<&'a str> {
@@ -57,11 +55,50 @@ pub fn search<'a>(pattern: &str, contents: &'a str) -> Vec<&'a str> {
         .collect()
 }
 
-pub fn search_insensitive<'a>(pattern: &str, contents: &'a str) -> Vec<&'a str> {
-    let pattern = pattern.to_lowercase();
+pub fn search_case_insensitive<'a>(
+    query: &str, 
+    contents: &'a str
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
 
-    contents
-        .lines()
-        .filter(|line| line.to_lowercase().contains(&pattern))
-        .collect()
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
